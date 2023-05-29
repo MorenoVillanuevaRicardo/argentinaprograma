51b59324ee7447d6b6a9551a53b7c6bcebc7959d
MorenoVillanuevaRicardo <asd123ricky@gmail.com>
2023-05-28 23:51:29 -0300

    primer commit

---
+++ practico0/multiplicacion.js
@@ -0,0 +1,4 @@
+function multiplicacion (a,b,c){
+    return a*b*c;
+}
+console.log("La multiplicacion de los 3 numeros es", multiplicacion(2,17,6));
---
+++ practico0/resta.js
@@ -0,0 +1,4 @@
+function resta(minuendo,sustraendo){
+    return minuendo-sustraendo;
+}
+console.log("La resta es",resta(10,2));
