addd97b7d8b6227f20903782465752b6a534525a
MorenoVillanuevaRicardo <asd123ricky@gmail.com>
2023-05-28 23:53:52 -0300

    Agrego el archivo conversionGrados.js

---
+++ practico0/conversionGrados.js
@@ -0,0 +1,5 @@
+function conversion(f){
+    return(f-32)*5/9;
+}
+console.log("La conversion del inciso A es",conversion(50),"grados Celsius");
+console.log("La conversion del inciso B es",conversion(75),"grados Celsius");
