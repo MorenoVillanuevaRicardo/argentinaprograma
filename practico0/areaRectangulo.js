00ce1ae2f5b30e603e6c7f0449bfea39af39cbcf
MorenoVillanuevaRicardo <asd123ricky@gmail.com>
2023-05-28 23:55:08 -0300

    Practico N0 finalizado

---
+++ practico0/areaRectangulo.js
@@ -0,0 +1,5 @@
+function areaRectangulo(base,altura){
+    return base*altura;
+}
+console.log ("El area del rectangulo del inciso A es igual a",areaRectangulo(5,3),"centimetros cuadrados");
+console.log ("El area del rectangulo del inciso B es igual a",areaRectangulo(12.5,5.3),"centimetros cuadrados");
---
+++ practico0/promedio3.js
@@ -0,0 +1,6 @@
+function promedio (x,y,z){
+    return (x+y+z)/3;
+}
+console.log("El promedio de los numeros del inciso A es",promedio(5,7,9));
+console.log("El promedio de los numeros del inciso B es",promedio(-2,-3,-8));
+console.log("El promedio de los numeros del inciso C es",promedio(-1.2,-5.3,-3.3));
