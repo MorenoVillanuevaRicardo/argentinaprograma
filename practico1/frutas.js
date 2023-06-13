aa90bd523414563dd4dc9ad64459828138eda044
MorenoVillanuevaRicardo <asd123ricky@gmail.com>
2023-06-13 19:23:31 -0300

    Agrego el punto 5

---
+++ practico1/frutas.js
@@ -0,0 +1,6 @@
+let frutas = [];
+frutas = ["naranja","uva","manzana","banana","pera"];
+let ultima = frutas[frutas.length-1];
+console.log("La ultima fruta agregada fue:",ultima);
+let primera = frutas[0];
+console.log("La primera fruta agregada fue:",primera);
