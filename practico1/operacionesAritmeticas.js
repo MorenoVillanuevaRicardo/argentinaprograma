3e4fac519862f51786f849eef89dfa4aa4fd5dac
MorenoVillanuevaRicardo <asd123ricky@gmail.com>
2023-06-12 21:41:23 -0300

    Agrego el punto 3

---
+++ practico1/operacionesAritmeticas.js
@@ -0,0 +1,10 @@
+let num1=4;
+let num2=7;
+let num3=5;
+let num4=1;
+let suma = num1 + num2;
+let resta = num3 - num4;
+let resultadoFinal = suma*resta;
+console.log(resultadoFinal);
+let esPar = (resultadoFinal % 2 === 0);
+console.log("Mis variables iniciales fueron", num1,",",num2,",",num3,"y",num4,".","La respuesta a verificar si el resultado final es para es:",esPar);
